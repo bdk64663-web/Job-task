55553e3762ef9cd5774fb438bf2982edc2e205f7
bdk64663-web <bdk64663@gmail.com>
2026-02-02 13:47:33 +0600

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get("/", (req, res) => {
   res.send("Backend running successfully 🚀");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });

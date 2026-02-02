0d3558df15a29cb3524fe04cb2ffae025f3201d7
bdk64663-web <bdk64663@gmail.com>
2026-02-02 13:46:00 +0600

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
+const TelegramBot = require("node-telegram-bot-api");
+
+const token = process.env.BOT_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", (msg) => {
+  bot.sendMessage(msg.chat.id, "Bot working ✅");
+});
